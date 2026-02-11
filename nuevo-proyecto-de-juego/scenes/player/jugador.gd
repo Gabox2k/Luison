2b6615d36a212a0cdc85d08123742c1d262f04c0
Gabriel Raidan <sforza1383@gmail,com>
2026-02-11 16:15:48 -0300

    se corrigio algunos errores

--- nuevo-proyecto-de-juego/scenes/player/jugador.gd
+++ nuevo-proyecto-de-juego/scenes/player/jugador.gd
@@ -1,10 +1,31 @@
 extends CharacterBody3D
 
-# Velocidad del jugador
-@export var velocidad := 6.0
+#configuracion
 
-func _physics_process(_delta):
-	# Hacer que siempre avance hacia adelante
-	velocity.z = -velocidad
-	# Mover al personaje
+@export var lane_distance := 2.0
+@export var forward_speed := 10.0
+@export var lane_change_speed := 10.
+
+#carril actual
+var current_lane := 1
+
+func _physics_process(delta):
+	#movimineto hacia adelante
+	velocity.z = forward_speed
+	
+	#cambiar carril con teclas
+	if Input.is_action_pressed("ui_left"):
+		current_lane -= 1
+		current_lane = clamp(current_lane, 0, 2) 
+	
+	if Input.is_action_pressed("ui_right"):
+		current_lane += 1
+		current_lane = clamp(current_lane, 0, 2)
+		
+	#Calcular posicion objetivo en x
+	var target_x = (current_lane - 1) * lane_distance
+	
+	#Movimimiento suave 
+	position.x = lerp(position.x, target_x, lane_change_speed * delta) 
+	
 	move_and_slide()
